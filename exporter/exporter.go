deab11a96b5b8a0bdea62a600f853e46ff870c4e
zhongmingFan <zhongmingFan@canway.net>
2023-02-24 20:48:12 +0800

    up

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -467,8 +467,6 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 		var up float64
 		if err := e.scrapeRedisHost(ch); err != nil {
 			e.registerConstMetricGauge(ch, "exporter_last_scrape_error", 1.0, fmt.Sprintf("%s", err))
-			e.registerConstMetricGauge(ch, "up", up)
-			return
 		} else {
 			up = 1
 			e.registerConstMetricGauge(ch, "exporter_last_scrape_error", 0, "")
