c6d7784df786be9cfb6b52d075bb62714f1c405e
zhongming Fan <zhongmingfan@canway.net>
2023-02-24 08:48:36 +0800

    feature: helm values

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -6,12 +6,12 @@ redis_versions=("7.0" "6.0" "5.0" "4.0.14" "3.2.9-r3")
 for version in "${redis_versions[@]}"; do
     version_suffix="v${version%%.*}"
 
-    helm install redis-$architecture-$version_suffix --namespace redis  -f ./values/standalone-values.yaml \
+    helm install redis-$architecture-$version_suffix --namespace redis  -f ./values/standalone_values.yaml \
     --set image.tag=$version \
     --set redis.podLabels.object_version=$version_suffix \
     ./redis
 done
 
 
-helm install redis-cluster-v7 --namespace redis -f ./values/cluster-values.yaml ./redis-cluster
-helm install redis-sentinel-v7 --namespace redis -f ./values/sentinel-values.yaml ./redis
+helm install redis-cluster-v7 --namespace redis -f ./values/cluster_values.yaml ./redis-cluster
+helm install redis-sentinel-v7 --namespace redis -f ./values/sentinel_values.yaml ./redis
