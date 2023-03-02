ad8731266d532ef4e7d651c47051d367d975b2f8
zhongming Fan <zhongmingfan@canway.net>
2023-03-02 13:39:08 +0800

    up

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # 部署监控对象
-redis_versions=("7.0" "6.0" "5.0" "4.0.14" "3.2.9-r3")
+object_versions=("7.0" "6.0" "5.0" "4.0.14" "3.2.9-r3")
+object=mysql
 
-for version in "${redis_versions[@]}"; do
+for version in "${object_versions[@]}"; do
     version_suffix="v${version%%.*}"
 
-    helm install redis-standalone-$version_suffix --namespace redis  -f ./values/standalone_values.yaml \
+    helm install redis-standalone-$version_suffix --namespace $object  -f ./values/standalone_values.yaml \
     --set image.tag=$version \
     --set redis.podLabels.object_version=$version_suffix \
     ./redis
 done
 
 
-helm install redis-cluster-v7 --namespace redis -f ./values/cluster_values.yaml ./redis-cluster
-helm install redis-sentinel-v7 --namespace redis -f ./values/sentinel_values.yaml ./redis
+helm install redis-cluster-v7 --namespace $object -f ./values/cluster_values.yaml ./redis-cluster
+helm install redis-sentinel-v7 --namespace $object -f ./values/sentinel_values.yaml ./redis
