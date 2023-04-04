38fd8bd7e6dcd95f65e4c08b515020194427ef42
zhongming Fan <zhongmingfan@canway.net>
2023-04-04 10:46:38 +0800

    pipeline: helm uninstall

--- weops/pipe-tools/charts/helm_uninstall.sh
+++ weops/pipe-tools/charts/helm_uninstall.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# 卸载监控对象
-NAMESPACE="redis"
+# 部署监控对象
+object_versions=("7.0" "6.0" "5.0" "4.0.14" "3.2.9-r3")
+object=redis
+
+for version in "${object_versions[@]}"; do
+    version_suffix="v${version%%.*}"
+
+    helm uninstall redis-standalone-"$version_suffix" --namespace $object
+done
+
+
+helm uninstall redis-cluster-v7 --namespace $object
+helm uninstall redis-sentinel-v7 --namespace $object
 
 # Uninstall Redis deployments
-for RELEASE in $(helm list -n $NAMESPACE --short)
+for RELEASE in $(helm list -n $object --short)
 do
   echo "Uninstalling $RELEASE ..."
-  helm uninstall -n $NAMESPACE $RELEASE
+  helm uninstall -n $object "$RELEASE"
 done
